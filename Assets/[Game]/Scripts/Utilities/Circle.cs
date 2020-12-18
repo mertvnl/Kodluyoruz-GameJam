df9d69540bef325cfd51175edb42dc1a7b78d706
mertvnl <41839620+mertvnl@users.noreply.github.com>
2020-12-19 02:39:28 +0300

    house colliders and laser added

--- Assets/[Game]/Scripts/Utilities/Circle.cs
+++ Assets/[Game]/Scripts/Utilities/Circle.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HouseRotateController : MonoBehaviour
+public class Circle : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = new Quaternion(transform.rotation.x, 90, transform.rotation.z, transform.rotation.w);
+        Destroy(this.gameObject, 3);
     }
 
 }
